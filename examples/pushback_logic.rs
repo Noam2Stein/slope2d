4f96d052cbe135a14c21960a2182af1d290fd0b8
Noam2Stein <noamsteindev@proton.me>
2026-07-12 18:17:16 +0300

    Expand the pushback_logic example

--- examples/pushback_logic.rs
+++ examples/pushback_logic.rs
@@ -13,14 +13,19 @@
 //! The initial idea for resolving collisions is shown in [`slow`] and [`fast`]
 //! is a non-iterative algorithm that achives the same result.
 
+use std::range::RangeInclusive;
+
+use ggmath::{Vec2A, Vec3A};
+use testbed::{KeyCode, run};
+
 fn main() {
-    let bodies = vec![1.0, 1.4, 2.0, 2.3, 5.4, 6.4];
-    let pushback_factors = vec![0.3, 0.7, 0.9, 0.1, 0.5];
+    let bodies = vec![-2.0, -1.0, -0.5, 1.7, 2.0, 2.5];
+    let pushback_factors = vec![0.5, 0.5, 0.1, 0.5, 0.5];
 
     let mut bodies_slow = bodies.clone();
-    slow(&mut bodies_slow, &pushback_factors);
+    approximate_solution(&mut bodies_slow, &pushback_factors);
     let mut bodies_fast = bodies.clone();
-    fast(&mut bodies_fast, &pushback_factors);
+    directly_compute_solution(&mut bodies_fast, &pushback_factors);
 
     if (0..bodies.len()).all(|i| (bodies_slow[i] - bodies_fast[i]).abs() < 1e-3) {
         dbg!(bodies_slow);
@@ -31,21 +36,77 @@ fn main() {
         println!();
         println!("not equal!");
     }
+
+    let mut bodies = bodies;
+    let mut idx = 0;
+
+    run(|ctx| {
+        if ctx.key_held(KeyCode::ArrowRight) {
+            bodies[idx] += 0.15;
+        }
+        if ctx.key_held(KeyCode::ArrowLeft) {
+            bodies[idx] -= 0.15;
+        }
+        if ctx.key_pressed(KeyCode::ArrowUp) {
+            idx += 1;
+        }
+        if ctx.key_pressed(KeyCode::ArrowDown) {
+            idx -= 1;
+        }
+
+        directly_compute_solution(&mut bodies, &pushback_factors);
+
+        for (i, body) in bodies.iter().enumerate() {
+            ctx.draw_rectangle(
+                Vec3A::new(i as f32 * 0.1, 0.5, 0.5),
+                Vec2A::splat(0.5),
+                Vec2A::new(*body as f32, 0.0),
+                0.0,
+            );
+        }
+    });
 }
 
-fn slow(bodies: &mut [f64], pushback_factors: &[f64]) {
+fn approximate_solution(bodies: &mut [f64], pushback_factors: &[f64]) {
     for _ in 0..100_000 {
         for (i, pushback_factor) in pushback_factors.iter().copied().enumerate() {
             let [body_1, body_2] = bodies.get_disjoint_mut([i, i + 1]).unwrap();
 
-            let intersection = 1.0 - (*body_2 - *body_1);
-            *body_1 -= intersection * (1.0 - pushback_factor);
-            *body_2 += intersection * pushback_factor;
+            let overlap = 1.0 - (*body_2 - *body_1);
+            if overlap > 0.0 {
+                *body_1 -= overlap * (1.0 - pushback_factor);
+                *body_2 += overlap * pushback_factor;
+            }
         }
     }
 }
 
-fn fast(bodies: &mut [f64], pushback_factors: &[f64]) {
+fn directly_compute_solution(bodies: &mut [f64], pushback_factors: &[f64]) {
+    for _ in 0..5 {
+        let mut ranges = Vec::<RangeInclusive<usize>>::new();
+        while ranges
+            .last()
+            .is_none_or(|&last| last.last < bodies.len() - 1)
+        {
+            let start = ranges.last().copied().map_or(0, |range| range.last + 1);
+            let last = (start + 1..bodies.len())
+                .take_while(|&last| bodies[last] - bodies[last - 1] <= 1.0)
+                .last()
+                .unwrap_or(start);
+
+            ranges.push((start..=last).into());
+        }
+
+        for range in ranges {
+            compute_single_chain(
+                &mut bodies[range],
+                &pushback_factors[range.start..range.last],
+            );
+        }
+    }
+}
+
+fn compute_single_chain(bodies: &mut [f64], pushback_factors: &[f64]) {
     let mut weight = 1.0;
     let mut sum = 0.0;
     let mut weight_sum = 0.0;
